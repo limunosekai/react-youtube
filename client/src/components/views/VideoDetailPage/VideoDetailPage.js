f246a92bc77a932212d7954d28572691a9bb4c09
limunosekai <powerlsh0103@gmail.com>
2021-05-03 21:11:06 +0900

    Add SideVideo in VideoDetailPage

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
+import SideVideo from './Sections/SideVideo';
 
 import './VideoDetailPage.css';
 
@@ -43,7 +44,9 @@ function VideoDetailPage(props) {
             {/* comments */}
           </div>
         </Col>
-        <Col lg={6} xs={24}></Col>
+        <Col lg={6} xs={24}>
+          <SideVideo />
+        </Col>
       </Row>
     );
   } else {
